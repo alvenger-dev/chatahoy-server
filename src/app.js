fcd2701c5bd4bc76ddd8583b84ba5f93ed9b6dce
alvenger-dev <arnelpvillegas@gmail.com>
2026-03-17 11:36:24 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,29 @@
+const express = require('express');
+const http = require('http');
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: process.env.PORT || 10000 });
+const app = express();
+const server = http.createServer(app);
 
-console.log("ChatAHOY server running...");
+// WebSocket server attached to HTTP server
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
-  console.log("Client connected");
+  console.log('Client connected');
 
   ws.on('message', (message) => {
-    console.log("Received:", message.toString());
+    console.log('Received:', message.toString());
 
-    // AUTO REPLY
-    ws.send("Echo: " + message.toString());
+    ws.send('Echo: ' + message.toString());
   });
+});
+
+app.get('/', (req, res) => {
+  res.send('ChatAHOY server is running');
+});
+
+const PORT = process.env.PORT || 10000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
