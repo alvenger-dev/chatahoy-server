b0552927f957a15f88cc9f826b75b5ee5c14e000
alvenger-dev <arnelpvillegas@gmail.com>
2026-03-17 11:01:08 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,40 +2,15 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: process.env.PORT || 10000 });
 
-let relay = null;
-let ship = null;
-
 console.log("ChatAHOY server running...");
 
 wss.on('connection', (ws) => {
+  console.log("Client connected");
 
-  ws.on('message', (msg) => {
-    const data = JSON.parse(msg);
+  ws.on('message', (message) => {
+    console.log("Received:", message.toString());
 
-    console.log("Received:", data);
-
-    if (data.type === "relay_connect") {
-      relay = ws;
-      console.log("HomeShip connected");
-    }
-
-    if (data.type === "ship_connect") {
-      ship = ws;
-      console.log("Ship connected");
-    }
-
-    if (data.type === "sms_received") {
-      if (ship) {
-        ship.send(JSON.stringify(data));
-      }
-    }
-
-    if (data.type === "send_sms") {
-      if (relay) {
-        relay.send(JSON.stringify(data));
-      }
-    }
-
+    // AUTO REPLY
+    ws.send("Echo: " + message.toString());
   });
-
 });
